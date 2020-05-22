6799ec12d538065eb41be59c62346ac3b4377daf
adeeba reaz <adeebarivera@outlook.com>
2020-05-22 20:38:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,40 +1,36 @@
+require 'pry'
 class Artist
-
-  attr_accessor :name, :songs
-  @@all =[]
-
-  def initialize(name)
-    @name = name
-    @songs = []
-    
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def save
-    @@all << self #saves the artist name to the all array
-  end
-
-  def self.all
-    @@all 
-  end
-
-  def self.find_or_create_by_name(artist_name)
-    found_artist = self.all.find {|artist| artist.name == artist_name}
-    if found_artist
-      found_artist
-    else
-      new_artist = self.new(artist_name)
-      new_artist.save
-      new_artist
+    attr_accessor :name
+    @@all = []
+    def initialize(name) #initializing with a name and saving the artist in @@all array.
+        @name = name
+        @@all << self
     end
-  end
-
-  def print_songs
-    @songs.each do |song|
-      puts song.name
+    def self.all #defining this as the reader for class variable @@all.
+        @@all
     end
-  end
+    def add_song (song) #song instance will be passed into this method and we're just assigning the artist to it. 
+        song.artist = self
+    end
+    def songs #
+       selecting all songs that belong to this instance of "Artist"
+        Song.all.select {|song| song.artist == self}
+    end
+    def self.find_or_create_by_name (name)
+        artist_name = self.all.find {|value| value.name == name} # search @@all for name passed and assign it to variable artist_name
+        if artist_name # if artist name is found/exists then just return it
+            artist_name
+        else # if it doesn't exist create a new artist instance with that name and then return it. 
+            artist_name = self.new(name)
+            artist_name
+        end
+    end
+    def print_songs # prints all songs that belong to this artist. Stupid way of doing it, could've been done by pust printing everything from "songs" method. 
+        Song.all.each do |song|
+            if song.artist == self
+                puts song.name
+            end
+        end
+    end
+
 end
